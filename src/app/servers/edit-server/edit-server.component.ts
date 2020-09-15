a03caca5f84bad01fe8d15c425920ff256791069
Engincan Varan <engincanvaran@Engincans-MacBook-Pro.local>
2020-09-15 18:02:37 +0300

    FineTuning Servers and subscribe it

--- src/app/servers/edit-server/edit-server.component.ts
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -30,8 +30,14 @@ export class EditServerComponent implements OnInit, CanComponentDeactivate {
       }
     );
     this.route.fragment.subscribe();
-
-    this.server = this.serversService.getServer(1);
+    
+    const id = +this.route.snapshot.params['id'];
+    this.server = this.serversService.getServer(id);
+    this.route.params.subscribe(
+      (params: Params) => {
+        this.server = this.serversService.getServer(+params['id']);
+      }
+    )
     this.serverName = this.server.name;
     this.serverStatus = this.server.status;
   }
